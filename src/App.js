92ba238ffd694d93129550dd65a4216b2fd52e12
hochan222 <201502766@hufs.ac.kr>
2019-09-09 18:58:38 +0900

    add sidebox

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import Navigator from './components/Navigator';
 import Body from './components/Body';
 import Footer from "./components/Footer/Footer";
 import Letter from './components/lib/letter';
-import Timer from './components/lib/Timer';
+import SideBox from './components/lib/SideBox';
 
 function App() {
   return (
     <>
-      <Timer/>
-      {/* <Letter/>
+      <SideBox/>
+      <Letter/>
       <Navigator/>
       <Body/>
-      <Footer/> */}
+      <Footer/>
     </>
   );
 }

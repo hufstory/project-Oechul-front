2a0eb053f56ac0b23b219d5c5f68afac03e45668
hochan222 <201502766@hufs.ac.kr>
2019-09-26 16:43:30 +0900

    Updates

---
+++ precache-manifest.9659aa38c3a92f08793c8bf304c4cdcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd6f84837fbbd4bb9ba7467450698d4",
+    "url": "/hufstory/project-Oechul-front/index.html"
+  },
+  {
+    "revision": "dc51224442f623815bba",
+    "url": "/hufstory/project-Oechul-front/static/css/main.a8664795.chunk.css"
+  },
+  {
+    "revision": "a81cc4976025be6c4769",
+    "url": "/hufstory/project-Oechul-front/static/js/2.202a4498.chunk.js"
+  },
+  {
+    "revision": "dc51224442f623815bba",
+    "url": "/hufstory/project-Oechul-front/static/js/main.62dc6f62.chunk.js"
+  },
+  {
+    "revision": "fc6c1cdc2c8377f2c866",
+    "url": "/hufstory/project-Oechul-front/static/js/runtime~main.223b8132.js"
+  }
+]);

9cc756d7d98b520a73275c059b756baa7e77f9b7
hochan222 <201502766@hufs.ac.kr>
2019-09-27 14:50:47 +0900

    modify Intro

--- src/components/lib/Timer/index.js
+++ src/components/lib/Timer/index.js
@@ -6,7 +6,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Sep 20, 2019 00:00:00').getTime(),
+let countDown = new Date('Oct 24, 2019 00:00:00').getTime(),
     x = setInterval(function() {
 
     let now = new Date().getTime(),

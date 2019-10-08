78a689c669c7ddc7323ae19fcba951634598cf83
woongbishin <kipper12335@gmail.com>
2019-10-08 16:46:50 +0900

    mobile

--- src/components/Body/Count/TinyCount/TinyCount.js
+++ src/components/Body/Count/TinyCount/TinyCount.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import './tiny_count.scss';
 
 class tiny_count extends Component{
+
     render() {
 
         return(
             <div>
-
+                <p>{this.props.title}</p>
             </div>
         );
     }

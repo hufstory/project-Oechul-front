4b1d7df0de04ee3c584d42adaaed52f222dde0ab
hochan222 <201502766@hufs.ac.kr>
2019-11-03 16:22:51 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-Oechul-front/precache-manifest.ed858fc06deaab756868c61957884337.js"
+  "/project-Oechul-front/precache-manifest.7c29a7ca70a930a48ba1afdb13fcfd6e.js"
 );
 
 self.addEventListener('message', (event) => {
